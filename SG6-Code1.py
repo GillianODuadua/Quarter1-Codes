8241c49b591ef941ed6c9d956d9f3b149e444352
GillianODuadua <gillianadina.duadua@smc.pshs.edu.ph>
2025-09-09 10:47:16 +0800

    Add files via upload

---
+++ SG6-Code1.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter your first number:"))
+num2 = float(input("Enter your second number:"))
+num3 = float(input("Enter your third number:"))
+sum = num1 + num2 + num3
+formatted_sum = f"{sum:.2f}"
+print ("The total of the three numbers is:", formatted_sum)
+    
